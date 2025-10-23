44b205f30070cfc4b3e7d9b5bda9e151d373900d
Anna Bystrova <annbystr@gmail.com>
2025-10-23 14:37:29 +0300

    add comments

--- tests/test_postman_echo.py
+++ tests/test_postman_echo.py
@@ -12,11 +12,13 @@ BASE_URL = "https://postman-echo.com/"
     ('post', 'get', 404)
 ])
 def test_postman_echo_status_codes(method, endpoint, expected_code):
+    """Проверка статус кодов ответов от сервера"""
     response = requests.request(method, BASE_URL + endpoint)
     assert response.status_code == expected_code
 
 
 def test_get_request_with_valid_query():
+    """Проверка GET запроса корректными параметрами"""
     queries = {
         "id": 12,
         "name": "test_user"
@@ -26,11 +28,13 @@ def test_get_request_with_valid_query():
         "name": "test_user"
     }
     response = requests.get("https://postman-echo.com/get", params=queries)
+    assert response.status_code == 200
     assert isinstance(response.json()['args'], dict)
     assert response.json()['args'] == expected
 
 
 def test_post_request_with_valid_query():
+    """Проверка POST запроса корректными параметрами"""
     queries = {
         "id": 12,
         "name": "test_user"
@@ -40,11 +44,13 @@ def test_post_request_with_valid_query():
         "name": "test_user"
     }
     response = requests.post("https://postman-echo.com/post", params=queries)
+    assert response.status_code == 200
     assert isinstance(response.json()['args'], dict)
     assert response.json()['args'] == expected
 
 
 def test_invalid_query_type():
+    """Проверка GET запроса с некорректным типом параметра"""
     response = requests.get("https://postman-echo.com/get", params={"id": None})
     assert response.status_code == 200
     assert response.json()["args"] == {}
